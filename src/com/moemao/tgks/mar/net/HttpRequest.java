fdd4c5acf4d49bf214492746273d73ebe0be7c22
Kane Blueriver <yanwuv4yue@users.noreply.github.com>
2015-07-02 02:31:37 +0800

    TGKS_MAR v1.0.3 2015.7.02 1、修改 删除不需要的请求接口，后续改为内部分离式设计，为负载均衡作准备

--- src/com/moemao/tgks/mar/net/HttpRequest.java
+++ src/com/moemao/tgks/mar/net/HttpRequest.java
@@ -8,7 +8,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.Semaphore;
 
 public class HttpRequest
@@ -17,15 +16,15 @@ public class HttpRequest
     
     private static final Semaphore semp = new Semaphore(1);
     
-    /**
-     * 向指定URL发送GET方法的请求
-     * 
-     * @param url 发送请求的URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return URL 所代表远程资源的响应结果
-     */
-    public String sendGet(String url, String param)
+    private boolean bDebug = false;
+    
+    public String sendPost(String url, String param)
     {
+        if (bDebug)
+        {
+            System.out.println(param);
+        }
+        
         String result = "";
         BufferedReader in = null;
         try
@@ -48,7 +47,7 @@ public class HttpRequest
                 System.out.println(key + "--->" + map.get(key));
             }
             // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
             String line;
             while ((line = in.readLine()) != null)
             {
@@ -78,130 +77,13 @@ public class HttpRequest
         try
         {
             byte[] bytes = result.getBytes();
-            result = new String(bytes, "UTF-8");
-        }
-        catch (Exception e)
-        {
-            
-        }
-        return result;
-    }
-    
-    /**
-     * 向指定 URL 发送POST方法的请求
-     * 
-     * @param url 发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return 所代表远程资源的响应结果
-     * @throws Exception 
-     */
-    public String sendPost(String url, String param) throws Exception
-    {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try
-        {
-            semp.acquire();
-            
-            URL realUrl = new URL(url);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 设置超时
-            conn.setReadTimeout(15000);
-            conn.setConnectTimeout(15000);
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            // update by ken 20150622 在流处理时就应该把编码格式定义好，这样才不会出现问题
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null)
-            {
-                result += line;
-            }
-            
-            Thread.sleep(SLEEP_TIME);
-        }
-        catch (Exception e)
-        {
-            System.out.println("发送 POST 请求出现异常！" + e);
-            e.printStackTrace();
-            this.notify();
-            throw e;
-        }
-        // 使用finally块来关闭输出流、输入流
-        finally
-        {
-            semp.release();
-            
-            try
-            {
-                if (out != null)
-                {
-                    out.close();
-                }
-                if (in != null)
-                {
-                    in.close();
-                }
-            }
-            catch (IOException ex)
-            {
-                ex.printStackTrace();
-            }
-        }
-        
-        try
-        {
-            byte[] bytes = result.getBytes();
             result = new String(bytes);
-            //System.out.println(result);
         }
         catch (Exception e)
         {
             
         }
-        
-        if (result.contains("�?,"))
-        {
-            result = result.replace("�?,", "\",");
-        }
-        if (result.contains("�?}"))
-        {
-            result = result.replace("�?}", "\"}");
-        }
-        if (result.contains("�?]"))
-        {
-            result = result.replace("�?]", "\"]");
-        }
-        
         return result;
     }
     
-    public static void main(String[] args)
-    {        
-        try
-        {
-            String url = "https://app.login.kairisei-ma.jp:443/Auth/login.php";
-            String paramStr = "{\"uuid\":\"" + UUID.randomUUID().toString() + "\",\"clver\":\"1\",\"os\":0,\"carrier\":3,\"market\":1,\"lang\":0,\"device\":\"iPhone5S\",\"token\":\"\"}";
-            String sr = new HttpRequest().sendPost(url, paramStr);
-            System.out.println(sr);
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
-    }
 }
